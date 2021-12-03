b3c21364bed82f5d681640f7286ca1831337cc0d
Saikumar Bandaru <saikumar.bandaru@zaggle.in>
2021-12-03 14:05:13 +0530

    spec written for one failure case and one success case

---
+++ app/controllers/api/bookings_controller.rb
@@ -0,0 +1,18 @@
+class Api::BookingsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def create
+    booking = Booking.new(booking_params)
+    if booking&.save
+      render json: { message: "Booking Created Successfully", errors: nil}, status: 200
+    else
+      render json: { message: "Failed To Create Booking", errors: booking&.errors&.messages}, status: 406
+    end
+    
+  end
+
+  private
+  def booking_params
+    params.permit(:name, :email, :mobile, :booking_date, :from_time, :to_time)
+  end
+end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  validates :name, presense: { message: "Name should not be empty"}
-  validates :email, presense: { message: "Email Should not be empty"}
-  validates :mobile, presense: { message: "Mobile Should not be empty"}
+  validates :name, :presence => { message: "Name should not be empty"}
+  validates :email, :presence => { message: "Email Should not be empty"}
+  validates :mobile, :presence => { message: "Mobile Should not be empty"}
   
 end
