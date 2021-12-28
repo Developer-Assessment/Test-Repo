ebfdc30d97e2e9226b341169e31a4f6df37bb689
Saikumar Bandaru <saikumar.bandaru@zaggle.in>
2021-12-28 20:01:58 +0530

    issue #9 - Fixed issues raised by rubocop

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
-  validates :name, :presence => { message: "Product name should not be empty"}
-  validates :code, :presence => { message: "Product code Should not be empty"}, :uniqueness => {:case_sensitive => false, message: "Product code already taken" }
+  validates :name, presence: { message: 'Product name should not be empty' }
+  validates :code, presence: { message: 'Product code Should not be empty' },
+                   uniqueness: { case_sensitive: false, message: 'Product code already taken' }
 end
