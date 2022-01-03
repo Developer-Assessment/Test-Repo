f4f342d255a84f9b289a8338a0bc04eb0395f2e6
Saikumar Bandaru <saikumar.bandaru@zaggle.in>
2022-01-03 07:50:12 +0530

    issue #11 - Made changes suggested by rubo cop

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,5 @@
 class Booking < ApplicationRecord
-  validates :name, :presence => { message: "Name should not be empty"}
-  validates :email, :presence => { message: "Email Should not be empty"}
-  validates :mobile, :presence => { message: "Mobile Should not be empty"}
-  
+  validates :name, presence: { message: 'Name should not be empty' }
+  validates :email, presence: { message: 'Email Should not be empty' }
+  validates :mobile, presence: { message: 'Mobile Should not be empty' }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_03_054233) do
+ActiveRecord::Schema.define(version: 2021_12_20_054525) do
 
   create_table "bookings", charset: "latin1", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2021_12_03_054233) do
     t.integer "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "mobile"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
   end
 
 end
